20e057ae05db7edcd5d79e05eef984733c18afba
Robert Doroftei <contact@robertd.ro>
2012-03-26 22:28:11 +0300

    nested API calls update ( call the API from within another method )

--- include/engine/api/calculator/sum.class.php
+++ include/engine/api/calculator/sum.class.php
@@ -1,5 +1,26 @@
 <?php
-class Request extends RequestAbstract{
+/*
+Api method class name should be:
+
+_namespace_method
+
+or better yet:
+
+str_replace("/","_","the_full_method")
+
+in this case
+
+/calculator/sum is what the api will call and the class is _calculator_sum
+
+the update is for nested API calls, so you can call other API methods inside others .. 
+stay focused when you do this as you can get to infinite loop .. hopefully you wont :P 
+
+note to self: maybe put some sort of break point when nested calls are done and can go on infinite loop - maybe not, TBD
+
+
+
+*/
+class _calculator_sum extends RequestAbstract{
 	protected $params = array(
 		"operand"=>YES
 	);
